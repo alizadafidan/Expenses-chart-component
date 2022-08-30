36a8c6e32d8035856a22014fe877122ec8be05cd
Fidan Alizada <alizadafidann@gmail.com>
2022-08-30 15:11:28 +0400

    add file

---
+++ script.js
@@ -0,0 +1,46 @@
+const graphItems = document.getElementsByClassName("graph");
+const amounts = document.getElementsByClassName("chart-amount");
+
+let chartData = null;
+
+fetch("./data.json")
+  .then((res) => res.json())
+  .then((data) => {
+    chartData = data;
+    console.log(chartData);
+  })
+  .then(() => makeChart(chartData));
+
+function makeChart(data) {
+  const date = new Date();
+  let day = date.getDay();
+  console.log(day);
+  let max;
+  max = data[0].amount;
+  console.log(max);
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].amount > max) {
+      max = data[i].amount;
+    }
+
+    amounts[i].style.visibility = "hidden";
+  }
+
+  let maxHeight = max + 10;
+  for (let i = 0; i < data.length; i++) {
+    graphItems[i].style.height = `${(data[i].amount / maxHeight) * 115}px`;
+    console.log(graphItems[i].style.height);
+    day == 0
+      ? (graphItems[6].style.backgroundColor = "hsl(186, 34%, 60%)")
+      : i == day - 1
+      ? (graphItems[i].style.backgroundColor = "hsl(186, 34%, 60%)")
+      : (graphItems[i].style.backgroundColor = "hsl(10, 79%, 65%)");
+    graphItems[i].addEventListener("mouseover", function () {
+      amounts[i].textContent = "$" + data[i].amount;
+      amounts[i].style.visibility = "visible";
+    });
+    graphItems[i].addEventListener("mouseleave", function () {
+      amounts[i].style.visibility = "hidden";
+    });
+  }
+}
